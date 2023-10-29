d5d1bcd3dfb5e4b34ba7b16e3a76247055b93417
BeeAwwad <aosalihu@gmail.com>
2023-10-29 07:29:27 +0100

    Debugging notification bar, 1st try

--- src/components/notification.jsx
+++ src/components/notification.jsx
@@ -1,9 +1,11 @@
 const Notification = ({ message, messageClass }) => {
   if (message === null) {
+    console.log("The value is null Awad");
     return null;
+  } else {
+    console.log(`The message is ${message} Awad`);
+    return <div className={messageClass}>{message}</div>;
   }
-
-  return <div className={messageClass}>{message}</div>;
 };
 
 export default Notification;
